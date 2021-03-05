8a05783ef8309927e35a504443e8d98f0b8f609b
Jodi Jade Trayler <jtrayler@student.cccs.edu>
2021-03-04 19:27:01 -0700

    Initial Import! Corrected Import with bug removed.

--- Assignment7/exercise1 (3).py
+++ Assignment7/exercise1 (3).py
@@ -1,9 +1,9 @@
 import arcpy
-arcpy.env.workspace = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Data\Admin\AdminData.gdb"
+arcpy.env.workspace = r"C:\Users\Owner\Documents\ArcGIS\Projects\ClassExerciseLesson#1\ClassExerciseLesson#1.gdb"
 arcpy.env.overwriteOutput = True
-arcpy.SelectLayerByAttribute_management("cities", "CLEAR_SELECTION")
+arcpy.SelectLayerByAttribute_management("Cities_db", "CLEAR_SELECTION")
 
-flayer = arcpy.MakeFeatureLayer_management("cities", "Cities_Layer")
+flayer = arcpy.MakeFeatureLayer_management("Cities_db", "Cities_Layer")
 
 qry = "POP1990 > 20000"
 arcpy.management.SelectLayerByAttribute(flayer, "NEW_SELECTION", qry)
@@ -11,12 +11,11 @@ arcpy.management.SelectLayerByAttribute(flayer, "NEW_SELECTION", qry)
 my_cnt = arcpy.management.GetCount(flayer)
 print(f"Selected cities is: {my_cnt}")
 
-arcpy.management.SelectLayerByLocation(flayer, "WITHIN_A_DISTANCE", "us_rivers", "10 miles", "SUBSET_SELECTION")
+arcpy.management.SelectLayerByLocation(flayer, "WITHIN_A_DISTANCE", "Rivers_db", "10 miles", "SUBSET_SELECTION")
 
 my_cnt = arcpy.management.GetCount(flayer)
 print(f"Selected cities is: {my_cnt}")
 
-flayer = arcpy.MakeFeatureLayer_management("cities", "Cities_Layer")
 field = 'POP1990'
 total = 0
 i = 1
